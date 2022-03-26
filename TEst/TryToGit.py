160403c67c7a7ed4409b9c03cae1bb40c4bc532b
GeorgeTsai0705 <george8575@gmail.com>
2022-03-26 17:40:31 +0800

    Add X-Z line to show

--- TEst/TryToGit.py
+++ TEst/TryToGit.py
@@ -4,5 +4,6 @@ import matplotlib.pyplot as plt
 x = list(range(200))
 y = [math.sin(i/50) for i in x]
 z = [math.cos(i/50) for i in x]
-plt.plot(x, y)
+plt.plot(x, y, color="r")
+plt.plot(x, z, color ="b")
 plt.show()
