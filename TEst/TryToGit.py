294ed2f0a2301e8c252904d54efe71958f984c58
GeorgeTsai0705 <67643437+GeorgeTsai0705@users.noreply.github.com>
2022-04-07 16:11:24 +0800

    Delete TEst directory

--- TEst/TryToGit.py
+++
@@ -1,9 +0,0 @@
-import math
-import matplotlib.pyplot as plt
-
-x = list(range(200))
-y = [math.sin(i/50) for i in x]
-z = [math.cos(i/50) for i in x]
-plt.plot(x, y, color="r")
-plt.plot(x, z, color ="b")
-plt.show()
