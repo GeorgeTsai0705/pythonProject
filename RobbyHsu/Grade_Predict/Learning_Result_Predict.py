d4dc787ffde5f263e81a79a8331fc7847cb60e58
GeorgeTsai0705 <george8575@gmail.com>
2022-04-07 16:07:20 +0800

    the initial version finished!

--- RobbyHsu/Grade_Predict/Learning_Result_Predict.py
+++ RobbyHsu/Grade_Predict/Learning_Result_Predict.py
@@ -1,8 +1,12 @@
-import sklearn
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import train_test_split
 
 # rate the different methods
-method = {"s":4, 'z':3, 'c':2, 'g':1, '0':-1, 'x':0.5, 'y':0.5}
+method = {"s": 6, 'z': 5, 'c': 4, 'g': 3, '0': 0, 'x': 0.5, 'y': 0.5}
+
 
 def load_data(filename):
     df = pd.read_csv(filename)
@@ -20,10 +24,47 @@ def load_data(filename):
 
     solution_list = []
     for ele in solution_data:
-        print(ele)
+        # change the solution code into number
+        solution_list.append(sum([method[ele[0]], method[ele[1]], method[ele[2]], method[ele[3]]]))
+
+    rank_data = df[['before_score_group', 'after_score_group']].values
+
+    rank_list = []
+
+    # if student's ability become worse maker "0", else maker "1" (not change or make progress
+    for ele in rank_data:
+        if ele[0] >= ele[1]:
+            rank_list.append(1)
+        else:
+            rank_list.append(0)
+    y = np.array([rank_list]).reshape(-1, 1)
+
+    X1 = np.array(draw_list).reshape(-1, 4)
+    X2 = np.array(solution_list).reshape(-1, 1)
+    X = np.hstack([X1, X2])
+
+    return X, y
+
+
+def SVM_training(X, y):
+    # normalize data
+    scaler = StandardScaler()
+    X_norm = scaler.fit_transform(X)
+
+    # cut the data into Train set and Test set
+    train_X, test_X, train_y, test_y = train_test_split(X_norm,y, random_state=111, train_size=0.8)
+
+    # use SVM to predict student's performance
+    SVM = LinearSVC(random_state=0, tol=1e-5)
+    SVM.fit(X=train_X, y= train_y.ravel())
+
+    # SVM model predict result
+    print(SVM.score(test_X, test_y.ravel()))
+
 
 def main():
-    load_data("Data.csv")
+    X, y = load_data("Data.csv")
+    SVM_training(X, y)
 
 
 if __name__ == '__main__':
