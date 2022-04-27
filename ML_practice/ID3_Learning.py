e43e41e7a73984c81ac0e21a17e385d809280af1
GeorgeTsai0705 <george8575@gmail.com>
2022-04-27 11:21:29 +0800

    ID3 predict model ver1

--- ML_practice/ID3_Learning.py
+++ ML_practice/ID3_Learning.py
@@ -1,6 +1,6 @@
-import numpy as np
 import math
-import copy
+from copy import deepcopy
+import numpy as np
 
 
 # import cPickle as Pickle
@@ -24,8 +24,12 @@ class ID3DTree(object):
         self.dataSet = recordlist
         self.labels = labels
 
+    def loadDataSet2(self, data, labels):
+        self.dataSet = data
+        self.labels = labels
+
     def train(self):
-        labels = copy.deepcopy(self.labels)
+        labels = deepcopy(self.labels)
         self.tree = self.buildTree(self.dataSet, labels)
 
     def maxCat(self, catelist):
@@ -47,8 +51,66 @@ class ID3DTree(object):
             return self.maxCate(cateList)
 
         # Core
-        bestFeat = self.getBestFest(dataSet)        # Return the best feature vector
+        bestFeat = self.getBestFest(dataSet)  # Return the best feature vector
         bestFeatLabel = labels[bestFeat]
-        tree = {bestFeatLabel:{}}
-        del(labels[bestFeat])                       # After picking up category of best feature, delete it from labels
+        tree = {bestFeatLabel: {}}
+        del (labels[bestFeat])  # After picking up category of best feature, delete it from labels
+
+        uniqueVals = set([data[bestFeat] for data in dataSet])
+        for value in uniqueVals:
+            subLabels = labels[:]
+            # seperate the dataset according to bestFeat & value
+            splitDataset = self.splitDataSet(dataSet, bestFeat, value)
+            subTree = self.buildTree(splitDataset, subLabels)        # build the subTree
+            tree[bestFeatLabel][value] = subTree
+        return tree
+
+    def getBestFest(self, dataSet):
+        # use this function to search the best feature vector
+        # the last col of dataSet is y value, so numFeatures must minus one
+        numFeatures = len(dataSet[0]) - 1
+
+        # use y value to calculate the baseEntropy
+        baseEntropy = self.computeEntropy(dataSet)
+        bestInfoGain = 0.0  # initialize the bestInfoGain
+        bestFeature = -1  # initialize the bestFeature
+
+        # outside loop: check each col of dataSet and calculate the best Feature vector
+        # take 'i' as an index for col of dataSet: range of 'i' is 0~(numFeature - 1 )
+        # Ex: category: Age has three kinds of value young, middle-age and young
+        # We calculate entropy for each values in each category
+        for i in range(numFeatures):  # take the ith col of dataSet
+            uniqueVals = set([data[i] for data in dataSet])  # remove duplicate values
+            newEntropy = 0
+            for value in uniqueVals:
+                subDataSet = self.splitDataSet(dataSet, i, value)
+                prob = len(subDataSet) / float(len(dataSet))
+                newEntropy += prob * self.computeEntropy(subDataSet)
+            infoGain = baseEntropy - newEntropy
+            if (infoGain > bestInfoGain):
+                bestInfoGain = infoGain
+                bestFeature = i
+        return bestFeature
+
+    def computeEntropy(self, dataSet):
+        # compute the Shannon Entropy
+        data_len = float(len(dataSet))
+        cateList = [data[-1] for data in dataSet]  # get the category result
+        # set a dictionary ( key: category, value: times of occurrence
+        items = dict([(i, cateList.count(i)) for i in cateList])
+        infoEntropy = 0.0  # initialize the Shannon Entropy
+        for key in items:  # Shannon Entropy: = -p*log2(p)
+            prob = float(items[key]) / data_len  # infoEntropy = -prob * log(prob,2)
+            infoEntropy -= prob * math.log(prob, 2)
+        return infoEntropy
+
+    # Delete the data row where the feature axis is located, and return the remaining data set
+    def splitDataSet(self, dataSet, axis, value):
+        rtnList = []
+        for featVec in dataSet:
+            if featVec[axis] == value:
+                rFeatVec = featVec[:axis]
+                rFeatVec = np.append(rFeatVec, featVec[axis + 1:])
+                rtnList.append(rFeatVec)
+        return rtnList
 
---
+++ ML_practice/ID3_practice.py
@@ -0,0 +1,15 @@
+from ID3_Learning import *
+from sklearn.datasets import load_iris
+import numpy as np
+
+data = load_iris()
+iris_data = data['data']
+iris_class = data['target']
+data = []
+
+label = ['sepal length', 'sepal width', 'petal length ', 'petal width']
+
+dtree = ID3DTree()
+dtree.loadDataSet2(data= np.insert(iris_data, 4, iris_class, axis=1), labels=label)
+dtree.train()
+print(dtree.tree)
