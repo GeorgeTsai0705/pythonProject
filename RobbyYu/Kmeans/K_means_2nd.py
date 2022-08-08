99b08e508dd55a4d5f4ef91d8a27dcbdcd7031f5
GeorgeTsai0705 <george8575@gmail.com>
2022-08-08 22:49:31 +0800

    update 0808

--- RobbyYu/Kmeans/K_means_2nd.py
+++ RobbyYu/Kmeans/K_means_2nd.py
@@ -9,28 +9,15 @@ import matplotlib.pyplot as plt
 
 # constant
 color = ["b", "g", "r", "y", "m", "c", "k", "lime", "brown", "pink"]
+target_school = "National Central University"
 
 # mode: with reputation / without reputation
-mode = "with reputation"
+mode = "without reputation"
 
 
-def sort_function(out_frame, in_frame):
-    college_name = list(in_frame["School_Name"])
-    labels = list(out_frame)
-    count_result = []
-    for name in college_name:
-        c = 0
-        for na in labels:
-            if name in list(out_frame[na]):
-                c += 1
-        count_result.append(c)
-
-    return count_result
-
-
-def prepare_data(in_frame):
+def prepare_asia_data(in_frame):
     # schools = in_frame[["School_Name"]]
-    df_temp = pd.DataFrame(data=in_frame[["School_Name"]])
+    df_temp = pd.DataFrame(data=in_frame[["School_Name", "Overall"]])
 
     # Indicator: Reputation
     np_list = in_frame[["Academic Reputation", "Employer Reputation "]].fillna(0).to_numpy()
@@ -55,43 +42,106 @@ def prepare_data(in_frame):
     return df_temp
 
 
-def main():
-    # load ranking data
-    df = pd.read_csv("Data/QS_Asia_v2.csv", encoding='unicode_escape')
-
+def qs_asia_fn(df):
     # prepare initial data
-    df_clean = prepare_data(df)
+    df_clean = prepare_asia_data(df)
 
     # choose mode
     labels = list(df_clean)
     if mode == "with reputation":
-        X_new = df_clean[[labels[1], labels[2], labels[3]]].to_numpy()
+        X_new = df_clean[[labels[2], labels[3], labels[4]]].to_numpy()
     else:
-        X_new = df_clean[[labels[2], labels[3]]].to_numpy()
+        X_new = df_clean[[labels[3], labels[4]]].to_numpy()
 
     # k-mean training process
     kmeans = KMeans(n_clusters=9, random_state=0).fit(X_new)
 
-    # Search Target
-    Target_Index = list(df_clean["School_Name"]).index("National Central University")
+    # Search Target and print the group of target
+    Target_Index = list(df_clean["School_Name"]).index(target_school)
     Target_values = X_new[Target_Index]
     Target_group = kmeans.predict([Target_values])[0]
+    print(Target_group)
 
     # Find each group's members
     temp_list = []
     for i in range(len(X_new)):
-        temp_list.append([kmeans.predict([X_new[i]])[0], list(df["School_Name"])[i], list(df["Rank_Asia"])[i]])
-    df_out = pd.DataFrame(data=temp_list, columns=["Group", "School_Name", "Rank_Asia"])
+        temp_list.append([kmeans.predict([X_new[i]])[0], list(df_clean["School_Name"])[i], list(df["Rank_Asia"])[i],
+                          list(df_clean["Overall"])[i]])
+
+    # Prepare output file for the k-mean result
+    df_out = pd.DataFrame(data=temp_list, columns=["Group", "School_Name", "Rank_Asia", 'Overall'])
     df_out.sort_values(by=["Group", "School_Name"], inplace=True)
     df_out.to_csv(path_or_buf="C_Result.csv")
+
+
+def prepare_the_asia_data(in_frame):
+    # schools = in_frame[["School_Name"]]
+    df_temp = pd.DataFrame(data=in_frame[["School_Name", "Overall"]])
+
+    # Indicator: Reputation
+    np_list = in_frame[["Industry income", "Research"]].fillna(0).to_numpy()
+    temp = np.array([x[0] * 0.075 + x[1] * 0.3 for x in np_list]).reshape(1, -1)
+    reputation_result = Normalizer(norm='max').fit_transform(X=temp)
+    df_temp = pd.concat([df_temp, pd.DataFrame(data=reputation_result[0], columns=["Reputation"])], axis=1)
+
+    # Indicator: Scholar
+    np_list = in_frame[["Citations", "Teaching"]].fillna(0).to_numpy()
+    temp = np.array([x[0] * 0.3 + x[1] * 0.25 for x in np_list]).reshape(1, -1)
+    scholar_result = Normalizer(norm='max').fit_transform(X=temp)
+    df_temp = pd.concat([df_temp, pd.DataFrame(data=scholar_result[0], columns=["Scholar"])], axis=1)
+
+    # Indicator: International
+    np_list = in_frame[["International outlook"]].fillna(0).to_numpy()
+    temp = np.array([x[0] * 0.08 for x in np_list]).reshape(1, -1)
+    international_result = Normalizer(norm='max').fit_transform(X=temp)
+    df_temp = pd.concat([df_temp, pd.DataFrame(data=international_result[0], columns=["International"])], axis=1)
+
+    return df_temp
+
+
+def the_asia_fn(df):
+    # prepare initial data
+    df_clean = prepare_the_asia_data(df)
+
+    # choose mode
+    labels = list(df_clean)
+    if mode == "with reputation":
+        X_new = df_clean[[labels[2], labels[3], labels[4]]].to_numpy()
+    else:
+        X_new = df_clean[[labels[3], labels[4]]].to_numpy()
+
+    # k-mean training process
+    kmeans = KMeans(n_clusters=9, random_state=0).fit(X_new)
+
+    # Search Target and print the group of target
+    Target_Index = list(df_clean["School_Name"]).index(target_school)
+    Target_values = X_new[Target_Index]
+    Target_group = kmeans.predict([Target_values])[0]
     print(Target_group)
 
+    # Find each group's members
+    temp_list = []
+    for i in range(len(X_new)):
+        temp_list.append([kmeans.predict([X_new[i]])[0], list(df_clean["School_Name"])[i], list(df["Rank_Asia"])[i],
+                          list(df_clean["Overall"])[i]])
 
-    """
-    # Final: output file
-    df_o.to_csv(path_or_buf="O_Result.csv", header=label_names)
-    
-    """
+    # Prepare output file for the k-mean result
+    df_out = pd.DataFrame(data=temp_list, columns=["Group", "School_Name", "Rank_Asia", 'Overall'])
+    df_out.sort_values(by=["Group", "School_Name"], inplace=True)
+    df_out.to_csv(path_or_buf="C_Result.csv")
+
+
+def main():
+    # load ranking data
+    data_type = "THE"  # THE/QS
+    df = pd.read_csv(f'Data/{data_type}_Asia.csv', encoding='unicode_escape')
+
+    if data_type == "QS":
+        # perform qs result analyse
+        qs_asia_fn(df)
+    elif data_type == "THE":
+        # perform the result analyse
+        the_asia_fn(df)
 
 
 if __name__ == '__main__':
