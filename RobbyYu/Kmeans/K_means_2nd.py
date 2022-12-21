9496337006b51da929b6af2b3e716bdc5c3c61f4
GeorgeTsai0705 <george8575@gmail.com>
2022-12-21 14:21:53 +0800

    新增東南亞(QSworld)資料分析，使用50/50權重

--- RobbyYu/Kmeans/K_means_2nd.py
+++ RobbyYu/Kmeans/K_means_2nd.py
@@ -15,8 +15,8 @@ target_school = "National Central University"
 np.set_printoptions(precision=3, suppress=False)
 # mode: A(Reputation vs Scholar)；B(Reputation vs International)；C(Scholar vs International)
 mode = "A"
-data_type = "THE"  # THE/QS
-k_cluster_num = 15
+data_type = "SEA"  # THE/QS/SEA
+k_cluster_num = 5
 
 def prepare_asia_data(in_frame):
     # schools = in_frame[["School_Name"]]
@@ -98,60 +98,6 @@ def qs_asia_fn(df):
             [i, df_out[mask]["Rank_Asia"].mean(0), df_out[mask]["Rank_Asia"].std(0), df_out[mask].count(0).values[0]])
     pd.DataFrame(data=statistic_data, columns=["Group", "Mean", "Std", 'Count']).to_csv(path_or_buf="S_Result.csv")
 
-def KMeanVisualize(data, classifier, mode):
-    h = 0.005
-
-    # Plot the decision boundary. For that, we will assign a color to each
-    x_min, x_max = data[:, 0].min() - 1,  data[:, 0].max() + 1
-    y_min, y_max = data[:, 1].min() - 1, data[:, 1].max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min,y_max,h))
-
-    # Obtain labels for each point in mesh
-    Z = classifier.predict(np.c_[xx.ravel(), yy.ravel()])
-
-    # Put the result into a color plot
-    Z = Z.reshape(xx.shape)
-    plt.figure(1,figsize=(7, 7))
-    plt.clf()
-    plt.imshow(
-        Z,
-        interpolation="nearest",
-        extent=(xx.min(), xx.max(), yy.min(), yy.max()),
-        cmap=plt.cm.Paired,
-        aspect="auto",
-        origin="lower",
-    )
-    plt.plot(data[:,0],data[:,1],"k.", markersize=2)
-    centroids = classifier.cluster_centers_
-
-    for ele in centroids:
-        plt.text(
-            ele[0],
-            ele[1],
-            s = str(classifier.predict([ele])+1),
-            c = 'white'
-        )
-
-    """
-    plt.scatter(
-        centroids[:,0],
-        centroids[:,1],
-        marker='x',
-        s = 169,
-        linewidths=3,
-        color='w',
-        zorder=10
-    )
-    """
-    if mode == "A":
-        plt.xlabel(xlabel="Reputation", size='x-large');plt.ylabel(ylabel="Scholar",size='x-large');
-    elif mode == "B":
-        plt.xlabel(xlabel="Reputation", size='x-large');plt.ylabel(ylabel="International", size='x-large');
-    else:
-        plt.xlabel(xlabel="Scholar", size='x-large');plt.ylabel(ylabel="International", size='x-large');
-    plt.xticks()
-    plt.yticks()
-    plt.show()
 
 def prepare_the_asia_data(in_frame):
     # schools = in_frame[["School_Name"]]
@@ -231,6 +177,81 @@ def the_asia_fn(df):
     pd.DataFrame(data=statistic_data, columns=["Group", "Mean", "Std", 'Count']).to_csv(path_or_buf="S_Result.csv")
 
 
+def prepare_SEasia_data(in_frame):
+    df_temp = pd.DataFrame(data=in_frame[["School_Name", "Location", "Overall"]])
+
+    # Indicator: Reputation
+    np_list = in_frame[["Academic Reputation", "Employer Reputation "]].fillna(0).to_numpy()
+    # Normal Weight
+    temp = np.array([x[0] * 0.5 + x[1] * 0.5 for x in np_list]).reshape(-1, 1)
+    reputation_result = StandardScaler().fit_transform(X=temp).reshape(1, -1)
+    df_temp = pd.concat([df_temp, pd.DataFrame(data=reputation_result[0], columns=["Reputation"])], axis=1)
+
+    # Indicator: Scholar
+    np_list = in_frame[["Faculty Student", "Citations per Faculty "]].fillna(0).to_numpy()
+    # Normal Weight
+    temp = np.array([x[0] * 0.5 + x[1] * 0.5 for x in np_list]).reshape(-1, 1)
+    scholar_result = StandardScaler().fit_transform(X=temp).reshape(1, -1)
+    df_temp = pd.concat([df_temp, pd.DataFrame(data=scholar_result[0], columns=["Scholar"])], axis=1)
+
+    # Indicator: International
+    np_list = in_frame[["International Faculty ", "International Students"]].fillna(0).to_numpy()
+    # Normal Weight
+    temp = np.array([x[0] * 0.5 + x[1] * 0.5 for x in np_list]).reshape(-1, 1)
+    international_result = StandardScaler().fit_transform(X=temp).reshape(1, -1)
+    df_temp = pd.concat([df_temp, pd.DataFrame(data=international_result[0], columns=["International"])], axis=1)
+
+    return df_temp
+
+def SEasia_fn(df):
+    # prepare initial data
+    df_clean = prepare_SEasia_data(df)
+
+    # choose mode
+    labels = list(df_clean)
+    for loop in range(3):
+        if loop == 0:
+            X_new =  df_clean[[labels[3], labels[4]]].to_numpy()
+        elif loop == 1:
+            X_new = df_clean[[labels[3], labels[5]]].to_numpy()
+        else:
+            X_new = df_clean[[labels[4], labels[5]]].to_numpy()
+
+        """
+        # 找出適當的分群數量
+        kmeans_list = [KMeans(n_clusters=k, random_state=123).fit(X_new) for k in range(3, 8)]
+        inertias = [model.inertia_ for model in kmeans_list]
+
+        # plot inertias vs K
+        plt.figure(1, figsize=(7, 4))
+        plt.clf()
+        plt.plot(list(range(3, 8)), inertias, 'bo-')
+        plt.xlabel("K")
+        plt.ylabel("inertias")
+        plt.figure(1, figsize=(7, 4))
+        plt.show()
+        """
+
+
+        # k-mean training process
+        kmeans = KMeans(n_clusters=k_cluster_num, random_state=0).fit(X_new)
+
+        # Find each group's members
+        temp_list = []
+        for i in range(len(X_new)):
+            temp_list.append(
+                [kmeans.predict([X_new[i]])[0] + 1, list(df_clean["School_Name"])[i], list(df["Rank"])[i],
+                 X_new[i][0], X_new[i][1]])
+
+        # Prepare output file for the k-mean result
+        df_out = pd.DataFrame(data=temp_list, columns=["Group", "School_Name", "Rank", 'A', 'B'])
+        df_out.sort_values(by=["Group", "School_Name"], inplace=True)
+        df_out.to_csv(path_or_buf="C_Result.csv")
+
+        # Draw Visualize Graph
+        KMeanVisualize(data=X_new, classifier=kmeans, mode=loop)
+
+
 def special_case(in_frame):
     np_list = in_frame[["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]].fillna(0).to_numpy()
     result = StandardScaler().fit_transform(X=np_list)
@@ -239,6 +260,61 @@ def special_case(in_frame):
         K = kmeans.predict([ele])[0]
         print(K)
 
+def KMeanVisualize(data, classifier, mode):
+    h = 0.005
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    x_min, x_max = data[:, 0].min() - 1,  data[:, 0].max() + 1
+    y_min, y_max = data[:, 1].min() - 1, data[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min,y_max,h))
+
+    # Obtain labels for each point in mesh
+    Z = classifier.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure(1,figsize=(7, 7))
+    plt.clf()
+    plt.imshow(
+        Z,
+        interpolation="nearest",
+        extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+        cmap=plt.cm.Paired,
+        aspect="auto",
+        origin="lower",
+    )
+    plt.plot(data[:,0],data[:,1],"k.", markersize=5)
+    centroids = classifier.cluster_centers_
+
+    for ele in centroids:
+        plt.text(
+            ele[0],
+            ele[1],
+            s = str(classifier.predict([ele])+1),
+            c = 'white'
+        )
+
+    """
+    plt.scatter(
+        centroids[:,0],
+        centroids[:,1],
+        marker='x',
+        s = 169,
+        linewidths=3,
+        color='w',
+        zorder=10 
+    )
+    """
+    print(mode)
+    if mode == "A" or mode == 0:
+        plt.xlabel(xlabel="Reputation", size='x-large');plt.ylabel(ylabel="Scholar",size='x-large');
+    elif mode == "B" or mode == 1:
+        plt.xlabel(xlabel="Reputation", size='x-large');plt.ylabel(ylabel="International", size='x-large');
+    else:
+        plt.xlabel(xlabel="Scholar", size='x-large');plt.ylabel(ylabel="International", size='x-large');
+    plt.xticks()
+    plt.yticks()
+    plt.show()
 
 def main():
     #df = pd.read_csv("Data/Taiwan_school.csv", encoding='unicode_escape')
@@ -253,6 +329,8 @@ def main():
     elif data_type == "THE":
         # perform the result analyse
         the_asia_fn(df)
+    elif data_type == "SEA":
+        SEasia_fn(df)
 
 
 if __name__ == '__main__':
