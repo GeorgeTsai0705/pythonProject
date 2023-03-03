798f832d73cdbfac74c64968b6157e4ac614a8d1
GeorgeTsai0705 <george8575@gmail.com>
2023-03-03 14:41:04 +0800

    修訂 v2

--- SENSOR2022/Classfier.py
+++ SENSOR2022/Classfier.py
@@ -26,16 +26,16 @@ def train_model_classifier(df, dc):
     scaler = StandardScaler().fit(x_data)
     x_scaled = scaler.transform(x_data)
 
-    X_train, X_test, y_train, y_test = train_test_split(x_scaled, y_data, test_size=0.30, random_state=7)
+    X_train, X_test, y_train, y_test = train_test_split(x_scaled, y_data, test_size=0.30, random_state=7, stratify=y_data)
     # kernal function: ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’
 
     # Use SVC to classify Result
-    classifier = SVC(C=1.1, kernel='linear', gamma='auto', degree=4, probability=True, random_state=15)
+    classifier = SVC(C=0.9, kernel='linear', gamma='auto', degree=4, probability=True, random_state=15)
     trained_out = model_process(classifier, X_train, X_test, y_train, y_test, "SVM Classifier", x_scaled, y_data)
     # check_result(clf=trained, X=x_scaled, y= y_data, l = label)
 
     # Use Random Forest
-    classifier = RandomForestClassifier(max_depth=4, criterion="entropy", n_estimators=90, random_state=123)
+    classifier = RandomForestClassifier(max_depth=2, criterion="entropy", n_estimators=80, random_state=123)
     trained = model_process(classifier, X_train, X_test, y_train, y_test, "Random Forest", x_scaled, y_data)
     # check_result(clf=trained, X=x_scaled, y=y_data, l = label)
 
@@ -82,17 +82,17 @@ def train_model_regression(df, dc):
     plt.figure(figsize=(10, 10))
 
     # Use SVR regression
-    regressor = SVR(C=0.8, kernel='linear', gamma='auto', degree=2, tol=0.0001)
+    regressor = SVR(C=0.2, kernel='linear', gamma='auto', degree=2, tol=0.0001)
     model_process_rg(regressor, X_train, X_test, y_train, y_test, "SVR Regression", 1, y_max, y_min)
 
     # Use Random Forest regression
-    regressor = RandomForestRegressor(n_estimators=30, max_depth=4)
+    regressor = RandomForestRegressor(n_estimators=20, max_depth=7)
     model_process_rg(regressor, X_train, X_test, y_train, y_test, "Random Forest Regression", 2, y_max, y_min)
 
     # Use MLPRegressor solver:{‘lbfgs’, ‘sgd’, ‘adam’} activation:{‘identity’, ‘logistic’, ‘tanh’, ‘relu’}
-    regressor = MLPRegressor(hidden_layer_sizes=(4, 2), activation="identity", solver="lbfgs",
+    regressor = MLPRegressor(hidden_layer_sizes=(5, 3), activation="identity", solver="lbfgs",
                              max_iter=10000, random_state=3, early_stopping=True)
-    model_process_rg(regressor, np.exp(X_train), np.exp(X_test), y_train, y_test, "MLP Regression", 3, y_max, y_min)
+    model_process_rg(regressor, X_train, X_test, y_train, y_test, "MLP Regression", 3, y_max, y_min)
 
     # Use GradientBoostingRegressor
     regressor = GradientBoostingRegressor(learning_rate=1, n_estimators=70, max_depth=2,
@@ -114,35 +114,59 @@ def model_process_rg(rg, X_train, X_test, y_train, y_test, method, plot_num, y_m
     print(f"Training Time: {end - start:.3f} s")
     print(f"R2: {r2}, MAE: {mean_absolute_error(percent2IU(y_test), percent2IU(rg.predict(X_test)))}")
 
+    # Check Low & High conc. performance
+    L_X=[]; L_y=[]; H_X=[];H_y=[]
+
+    for i in range(len(y_test)):
+        if y_test[i] < 0.35:
+            L_X.append(X_test[i])
+            L_y.append(y_test[i])
+        else:
+            H_X.append(X_test[i])
+            H_y.append(y_test[i])
+    print(f"Low R2: {mean_absolute_error(percent2IU(L_y), percent2IU(rg.predict(L_X))):.3f}")
+    print(f"High R2: {mean_absolute_error(percent2IU(H_y), percent2IU(rg.predict(H_X))):.3f}")
+
     # Plot setting
     plt.subplot(2, 2, plot_num)
     plt.plot(percent2IU(y_test),  percent2IU(rg.predict(X_test)), linestyle='', marker='o', mfc='b', ms='4')
     plt.plot([200, 0], [200, 0], linestyle='--', lw=2, c="gray")
-    plt.xlim([0, 200])
-    plt.ylim([0, 200])
+    plt.xlim([0, 220])
+    plt.ylim([0, 220])
+    plt.xlabel("Measured neutralizing antibody (IU/mL)")
+    plt.ylabel("Predicted neutralizing antibody (IU/mL)")
     plt.fill_between(x=[0, 200], y1=[20, 220], y2=[-20, 180], color='C0', alpha=0.1, label='Area')
     plt.title(method)
+
+    output = pd.DataFrame(data= [percent2IU(y_test), percent2IU(rg.predict(X_test))])
+    output.to_csv(f"{method}.csv")
 
     return rg
 
 
 def model_process(clf, X_train, X_test, y_train, y_test, method, x_scaled, y_data):
+    # 開始時間
     start = time.process_time()
+
+    # 訓練機器學習模型
     clf.fit(X_train, y_train)
+
+    # 計算測試集上的準確率、精確率、召回率和F1值
     accuracy = clf.score(X_test, y_test)
-    result = precision_recall_fscore_support(y_test, clf.predict(X_test), average='macro')
-    end = time.process_time()
+    precision, recall, f1, _ = precision_recall_fscore_support(y_test, clf.predict(X_test), average='macro')
+
+    # 計算混淆矩陣
     conf_matrix = confusion_matrix(y_test, clf.predict(X_test))
-    print("*" * 35)
-    print(f"{method} training result")
-    print(f"Training Time: {end - start:.3f} s")
-    print(
-        f"Accuracy: {accuracy * 100:.1f}, Precision: {result[0] * 100:.1f}, Recall: {result[1] * 100:.1f}, F1:{result[2] * 100:.1f}")
+
+    # 顯示訓練結果
+    print(f"{method} - Training Time: {time.process_time() - start:.3f} s, Accuracy: {accuracy * 100:.1f}, Precision: {precision * 100:.1f}, Recall: {recall * 100:.1f}, F1:{f1 * 100:.1f}")
     print("Confusion Matrix: ")
     print(conf_matrix)
 
-    cross_val = cross_val_score(clf, x_scaled, y_data, cv=5)
-    print(f"{method} cross avg: {np.average(cross_val)}")
+    # 計算並顯示交叉驗證分數
+    print(f"{method} - Cross-Validation Score: {np.average(cross_val_score(clf, x_scaled, y_data, cv=5))}")
+
+    # 返回已經訓練好的機器學習模型
     return clf
 
 
@@ -183,7 +207,7 @@ def special_reg(df, dc):
     scaler = StandardScaler().fit(x_data)
     x_scaled = scaler.transform(x_data)
 
-    X_train, X_test, y_train, y_test = train_test_split(x_scaled, y_data_c, test_size=0.3, random_state=1, stratify=y_data_c)
+    X_train, X_test, y_train, y_test = train_test_split(x_scaled, y_data_c, test_size=0.3, random_state=10, stratify=y_data_c)
     # kernal function: ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’
     # Use SVC to classify Result
     classifier = SVC(C=1.0, kernel='linear', gamma='auto', degree=4, probability=True, random_state=1)
@@ -233,20 +257,22 @@ def special_reg(df, dc):
     plt.plot([200, 0], [200, 0], linestyle='--', lw=2, c="gray")
     plt.xlim([0, 200])
     plt.ylim([0, 200])
-    plt.xlabel("Measured")
-    plt.ylabel("Predicted")
+    plt.xlabel("Measured neutralizing antibody (IU/mL)")
+    plt.ylabel("Predicted neutralizing antibody (IU/mL)")
     plt.fill_between(x=[0, 200], y1=[20, 220], y2=[-20, 180], color='C0', alpha=0.1, label='Area')
     plt.title('Improvement')
     plt.show()
 
+    output = pd.DataFrame(data= [percent2IU(y_test), percent2IU(y_result)])
+    output.to_csv("Output.csv")
 
 def main():
     # load data from csv file
     df = pd.read_csv('Temp_Alld_tline.csv', encoding='unicode_escape')
     dc = pd.read_csv('cPassResult.csv', encoding='unicode_escape')
-    #trained_clf, newX, newY = train_model_classifier(df, dc)
+    trained_clf, newX, newY = train_model_classifier(df, dc)
     #special_reg(df, dc)
-    train_model_regression(df, dc)
+    #train_model_regression(df, dc)
 
 
 if __name__ == '__main__':
