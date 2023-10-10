1b79eebd73985ab2c10f6d348b9fcb0dae630d43
GeorgeTsai0705 <george8575@gmail.com>
2023-10-10 13:40:40 +0800

    Modify UI content

--- Spectrochip_USBver_Sample/OV9281_USB_realtime.py
+++ Spectrochip_USBver_Sample/OV9281_USB_realtime.py
@@ -4,131 +4,175 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image, ImageTk
 import cv2
-import time  # Import time for sleep
-import threading  # Import threading for async operations
+import time
+import threading
+
 
 class VideoAnalyzer(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Real-Time Video Analyzer")
 
-        self.cap = None  # Initialize the cap object as None
-
-        self.mean_values = None
+        # Initial setup
+        self.cap = None
         self.coefficients = [0.00002946, 0.6312, 284.4]
+        self.original_image = None
+        self.mean_values = None
 
+        # Populate and display available cameras
         self.available_cameras = self.find_available_cameras()
-        self.cmb_cameras = ttk.Combobox(self, values=self.available_cameras)
-        self.cmb_cameras.pack(pady=10)
+        self.setup_ui_elements()
+
+        # Start capturing
+        self.capture_frame()
+
+    def setup_ui_elements(self):
+        """Initialize and display the UI components."""
+
+        # Camera selection frame
+        self.camera_frame = tk.Frame(self)
+        self.camera_frame.pack(pady=10)
+
+        # Label for camera selection
+        self.lbl_choose_cam = tk.Label(self.camera_frame, text="Choose Cam:")
+        self.lbl_choose_cam.pack(side=tk.LEFT, padx=5)
+
+        # Combobox for camera selection
+        self.cmb_cameras = ttk.Combobox(self.camera_frame, values=self.available_cameras)
+        self.cmb_cameras.pack(side=tk.LEFT)
         if self.available_cameras:
-            self.cmb_cameras.current(0)  # Set default selection to the first available camera
+            self.cmb_cameras.current(0)
         self.cmb_cameras.bind("<<ComboboxSelected>>", self.on_camera_selection)
 
-        self.mean_values = None
-        self.coefficients = [0.00002946, 0.6312, 284.4]
-
+        # Image display
         self.img_label = tk.Label(self)
         self.img_label.pack(pady=20)
 
-        self.lbl_start_row = tk.Label(self, text="Start Row:")
-        self.lbl_start_row.pack(pady=5)
+        # Frame for start and end rows
+        self.row_frame = tk.Frame(self)
+        self.row_frame.pack(pady=10)
 
-        self.entry_start_row = tk.Entry(self, width=10)
-        self.entry_start_row.pack(pady=5)
+        # Label and Entry for start row
+        self.lbl_start_row = tk.Label(self.row_frame, text="Start Row:")
+        self.lbl_start_row.grid(row=0, column=0, padx=5)
+        self.entry_start_row = tk.Entry(self.row_frame, width=10)
+        self.entry_start_row.grid(row=0, column=1, padx=5)
         self.entry_start_row.insert(0, '230')
 
-        self.lbl_end_row = tk.Label(self, text="End Row:")
-        self.lbl_end_row.pack(pady=5)
-
-        self.entry_end_row = tk.Entry(self, width=10)
-        self.entry_end_row.pack(pady=5)
+        # Label and Entry for end row
+        self.lbl_end_row = tk.Label(self.row_frame, text="End Row:")
+        self.lbl_end_row.grid(row=0, column=2, padx=5)
+        self.entry_end_row = tk.Entry(self.row_frame, width=10)
+        self.entry_end_row.grid(row=0, column=3, padx=5)
         self.entry_end_row.insert(0, '250')
 
-        self.btn_plot = tk.Button(self, text="Plot", command=self.plot_graph)
-        self.btn_plot.pack(pady=20)
+        # Buttons frame
+        self.buttons_frame = tk.Frame(self)
+        self.buttons_frame.pack(pady=10)
 
-        self.btn_save_spectrum = tk.Button(self, text="Save Spectrum", command=self.save_spectrum)
-        self.btn_save_spectrum.pack(pady=20)
+        # Button for plotting
+        self.btn_plot = tk.Button(self.buttons_frame, text="Plot", command=self.plot_graph)
+        self.btn_plot.pack(side=tk.LEFT, padx=5)
+
+        # Button for saving spectrum
+        self.btn_save_spectrum = tk.Button(self.buttons_frame, text="Save Spectrum", command=self.save_spectrum)
+        self.btn_save_spectrum.pack(side=tk.LEFT, padx=5)
+
+        # Quit button
+        self.btn_quit = tk.Button(self.buttons_frame, text="Quit", command=self.on_closing)
+        self.btn_quit.pack(side=tk.LEFT, padx=5)
 
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-        self.capture_frame()
-
     def find_available_cameras(self):
+        """Identify available cameras."""
         available_cameras = []
         for i in range(10):  # Check indices 0 to 9
             cap = cv2.VideoCapture(i)
             if cap.isOpened():
                 available_cameras.append(f"USB {i}")
-                cap.release()  # Release the camera immediately after checking
+                cap.release()
         return available_cameras
 
-    def on_camera_selection(self, event):
-        # Run the camera selection in a separate thread to avoid UI freezing
+    def on_camera_selection(self, _):
+        """Handle camera selection and start capturing asynchronously."""
         threading.Thread(target=self.async_camera_selection).start()
 
     def async_camera_selection(self):
-        camera_index = int(self.cmb_cameras.get().split()[-1])  # Extract camera index from selection
+        """Async handling of camera selection."""
+        camera_index = int(self.cmb_cameras.get().split()[-1])
 
         if self.cap is not None and self.cap.isOpened():
-            self.cap.release()  # Release the previous camera if it's opened
-            time.sleep(0.5)  # Small delay to ensure previous camera is released
+            self.cap.release()
+            time.sleep(0.5)
 
         self.cap = cv2.VideoCapture(camera_index)
-        self.capture_frame()  # Start capturing from the newly selected camera
+        self.capture_frame()
+
+    def on_closing(self):
+        """Handle app closing event."""
+        if self.cap:
+            self.cap.release()
+        self.destroy()
 
     def get_row_range(self):
+        """Get start and end row values."""
         try:
-            start_row = int(self.entry_start_row.get())
-            end_row = int(self.entry_end_row.get())
-            return start_row, end_row
+            return int(self.entry_start_row.get()), int(self.entry_end_row.get())
         except ValueError:
-            tk.messagebox.showerror("Error", "Please enter valid numbers for Start Row and End Row.")
-            # Use default values if row values are not valid
-            return 230, 250
+            messagebox.showerror("Error", "Please enter valid numbers for Start Row and End Row.")
+            return 230, 250  # Default values
 
     def capture_frame(self):
-        if self.cap is None or not self.cap.isOpened():  # Check if a camera is selected and opened
+        """Capture video frames and display in UI."""
+        if not (self.cap and self.cap.isOpened()):
             return
-        ret, frame = self.cap.read()  # Read a frame from the video
+
+        ret, frame = self.cap.read()
         if ret:
-            row_range = self.get_row_range()
-            if row_range is not None:
-                start_row, end_row = row_range
-            else:
-                # Use default values if row values are not valid
-                start_row, end_row = 0, frame.shape[0] - 1
-
-            # Create a copy of the frame for drawing
+            start_row, end_row = self.get_row_range()
             frame_copy = frame.copy()
-
-            # Draw a red rectangle for the ROI on the copy
             cv2.rectangle(frame_copy, (0, start_row), (frame.shape[1] - 1, end_row), (0, 0, 255), 1)
-
             self.original_image = Image.fromarray(frame)
             self.update_image_label(Image.fromarray(cv2.cvtColor(frame_copy, cv2.COLOR_BGR2RGB)))
 
-        if self.winfo_exists():  # Check if the window still exists
-            self.after(10, self.capture_frame)  # If the window still exists, capture a frame every 10 milliseconds
+        if self.winfo_exists():
+            self.after(10, self.capture_frame)
 
     def update_image_label(self, img):
-        img = img.resize((640, 480))  # You can adjust the size if needed
+        """Update image in the tkinter UI."""
+        img = img.resize((640, 480))
         self.tk_image = ImageTk.PhotoImage(img)
         self.img_label.config(image=self.tk_image)
 
+    def plot_graph(self):
+        """Plot the spectrum graph based on selected rows."""
+        start_row, end_row = self.get_row_range()
+        if not self.original_image:
+            return
+
+        img_data = np.array(self.original_image)
+        if 0 <= start_row < img_data.shape[0] and 0 <= end_row < img_data.shape[0] and start_row <= end_row:
+            indexes = np.arange(self.original_image.width)
+            wavelengths = self.coefficients[0] * indexes ** 2 + self.coefficients[1] * indexes + self.coefficients[2]
+            selected_rows = img_data[start_row:end_row + 1]
+            self.mean_values = np.mean(selected_rows, axis=(0, 2))
+            plt.figure(figsize=(10, 5))
+            plt.plot(wavelengths, self.mean_values)
+            plt.title(f"Average values of selected rows (Start Row: {start_row}, End Row: {end_row})")
+            plt.xlabel("Wavelength (nm)")
+            plt.ylabel("Average Value")
+            plt.show()
+        else:
+            messagebox.showerror("Error", "Invalid row values!")
+
     def save_spectrum(self):
-        row_range = self.get_row_range()
-        if row_range is None:
-            return
-
+        """Save the spectrum data."""
         if self.mean_values is None or self.original_image is None:
-            tk.messagebox.showerror("Error", "Please plot the spectrum first!")
+            messagebox.showerror("Error", "Please plot the spectrum first!")
             return
 
-        # Calculate the column indexes of the image
         indexes = np.arange(self.original_image.width)
-
-        # Convert indexes to wavelengths using the quadratic equation
         wavelengths = self.coefficients[0] * indexes ** 2 + self.coefficients[1] * indexes + self.coefficients[2]
 
         save_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt")])
@@ -136,46 +180,17 @@ class VideoAnalyzer(tk.Tk):
             return
 
         with open(save_path, 'w') as f:
-            f.write("Wavelength(nm)\tIntensity\n")  # Write the header
+            f.write("Wavelength(nm)\tIntensity\n")
             for w, i in zip(wavelengths, self.mean_values):
-                f.write(f"{format(w, '.2f')}\t{format(i, '.2f')}\n")  # Output wavelength and intensity simultaneously
+                f.write(f"{format(w, '.2f')}\t{format(i, '.2f')}\n")
         print(f"Spectrum saved to {save_path}")
 
-    def plot_graph(self):
-        row_range = self.get_row_range()
-        if row_range is None or not self.original_image:
-            return
-
-        start_row, end_row = row_range
-        img_data = np.array(self.original_image)
-
-        # Check the validity of row values
-        if 0 <= start_row < img_data.shape[0] and 0 <= end_row < img_data.shape[0] and start_row <= end_row:
-            indexes = np.arange(self.original_image.width)
-            # Convert indexes to wavelengths using the quadratic equation
-            wavelengths = self.coefficients[0] * indexes ** 2 + self.coefficients[1] * indexes + self.coefficients[2]
-
-            # Calculate the mean values for the selected rows
-            selected_rows = img_data[start_row:end_row + 1]
-            mean_values = np.mean(selected_rows, axis=(0, 2))
-
-            # Store the mean_values to self after plotting
-            self.mean_values = mean_values
-
-            # Plot
-            plt.figure(figsize=(10, 5))
-            plt.plot(wavelengths, mean_values)
-            title = f"Average values of selected rows (Start Row: {start_row}, End Row: {end_row})"
-            plt.title(title)
-            plt.xlabel("Wavelength (nm)")
-            plt.ylabel("Average Value")
-            plt.show()
-        else:
-            tk.messagebox.showerror("Error", "Invalid row values!")
-
     def on_closing(self):
-        self.cap.release()  # Release the video capture object
-        self.destroy()  # Close the tkinter window
+        """Handle app closing event."""
+        if self.cap:
+            self.cap.release()
+        self.destroy()
+
 
 if __name__ == '__main__':
     app = VideoAnalyzer()
