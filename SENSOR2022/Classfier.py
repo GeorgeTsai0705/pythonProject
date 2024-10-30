94432e575636c98d7efd0c3822df9ef7af2ac4e1
GeorgeTsai0705 <george8575@gmail.com>
2024-10-30 16:33:37 +0800

    移動路徑

--- SENSOR2022/Classfier.py
+++ SENSOR2022/Classfier.py
@@ -15,91 +15,66 @@ from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier,
 from sklearn.metrics import confusion_matrix, precision_recall_fscore_support, r2_score, mean_absolute_error
 
 
-def train_model_classifier(df, dc):
-    label = list(df)[1:]
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import pandas as pd
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.svm import SVC, SVR
+from sklearn.neural_network import MLPClassifier, MLPRegressor
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier, RandomForestRegressor, \
+    GradientBoostingRegressor
+from sklearn.metrics import confusion_matrix, precision_recall_fscore_support, r2_score, mean_absolute_error
+
+# Global constants
+TEST_SIZE = 0.3
+RANDOM_STATE = 7
+GROUP_NUM = 50
+
+def load_data(file_path):
+    return pd.read_csv(file_path, encoding='unicode_escape')
+
+def process_data(df):
     Wavelength = df["Wavelength"].to_numpy()
     df.drop(["Wavelength"], axis=1, inplace=True)
-    Wavelength, data = reduce_dimension(Wavelength, df.to_numpy(), 50)
-    x_data = data
-    y_data = dc.to_numpy()[1][1:].astype(int)
+    Wavelength, data = reduce_dimension(Wavelength, df.to_numpy(), GROUP_NUM)
+    return Wavelength, data
 
+def scale_data(x_data):
     scaler = StandardScaler().fit(x_data)
-    x_scaled = scaler.transform(x_data)
+    return scaler.transform(x_data)
 
-    X_train, X_test, y_train, y_test = train_test_split(x_scaled, y_data, test_size=0.30, random_state=7, stratify=y_data)
-    # kernal function: ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’
+def reduce_dimension(lambada, data, group_num):
+    loop_num = len(lambada) // group_num
+    new_lambada = [np.average(lambada[i * group_num:(i + 1) * group_num]) for i in range(loop_num)]
+    new_data = [[np.average(sample[i * group_num:(i + 1) * group_num]) for i in range(loop_num)] for sample in data.transpose()]
+    return np.array(new_lambada), np.array(new_data)
 
-    # Use SVC to classify Result
-    classifier = SVC(C=0.9, kernel='linear', gamma='auto', degree=4, probability=True, random_state=15)
-    trained_out = model_process(classifier, X_train, X_test, y_train, y_test, "SVM Classifier", x_scaled, y_data)
-    # check_result(clf=trained, X=x_scaled, y= y_data, l = label)
+def percent2IU(a):
+    return np.exp(a*3.66)*6.0175 if not isinstance(a, list) else [np.exp(x*3.66)*6.0175 for x in a]
 
-    # Use Random Forest
-    classifier = RandomForestClassifier(max_depth=2, criterion="entropy", n_estimators=80, random_state=123)
-    trained = model_process(classifier, X_train, X_test, y_train, y_test, "Random Forest", x_scaled, y_data)
-    # check_result(clf=trained, X=x_scaled, y=y_data, l = label)
+def model_process(clf, X_train, X_test, y_train, y_test, method, x_scaled, y_data):
+    start = time.process_time()
+    clf.fit(X_train, y_train)
+    accuracy = clf.score(X_test, y_test)
+    precision, recall, f1, _ = precision_recall_fscore_support(y_test, clf.predict(X_test), average='macro')
+    conf_matrix = confusion_matrix(y_test, clf.predict(X_test))
 
-    # Use MLPClassifier
-    classifier = MLPClassifier(hidden_layer_sizes=(5, 2), random_state=5, max_iter=8000, early_stopping=True,
-                               validation_fraction=0.25, solver="lbfgs", activation="identity")
-    trained = model_process(classifier, X_train, X_test, y_train, y_test, "MLPClassifier", x_scaled, y_data)
-    # check_result(clf=trained, X=x_scaled, y=y_data, l = label)
+    print(f"{method} - Training Time: {time.process_time() - start:.3f} s, "
+          f"Accuracy: {accuracy * 100:.1f}, "
+          f"Precision: {precision * 100:.1f}, "
+          f"Recall: {recall * 100:.1f}, "
+          f"F1:{f1 * 100:.1f}")
+    print("Confusion Matrix: ")
+    print(conf_matrix)
+    print(f"{method} - Cross-Validation Score: {np.average(cross_val_score(clf, x_scaled, y_data, cv=5))}")
 
-    # Use Gradient Boosting Classifier
-    classifier = GradientBoostingClassifier(n_estimators=30, learning_rate=0.005, max_depth=5,
-                                            random_state=11, loss='deviance')
-    model_process(classifier, X_train, X_test, y_train, y_test, "Gradient Boosting Classifier", x_scaled,
-                  y_data)
-    # check_result(clf=trained, X=x_scaled, y=y_data, l = label)
-
-    return trained_out, x_scaled, y_data
-
-
-def check_result(clf, X, y, l):
-    for i in range(len(X)):
-        o = clf.predict([X[i]])
-        if o[0] != y[i]:
-            print(f"{l[i]}: ", f"Predict-> {o[0]}", f"Label-> {y[i]}")
-
-
-def train_model_regression(df, dc):
-    list(df)[1:]
-    Wavelength = df["Wavelength"].to_numpy()
-    df.drop(["Wavelength"], axis=1, inplace=True)
-    Wavelength, data = reduce_dimension(Wavelength, df.to_numpy(), 50)
-    x_data = data
-    y_data = dc.to_numpy()[0][1:].astype("float16")
-    y_data_c = dc.to_numpy()[1][1:].astype("int")
-    scaler = StandardScaler().fit(x_data)
-    x_scaled = scaler.transform(x_data)
-
-    y_max = max(y_data)
-    y_min = min(y_data)
-    #y_data = (y_data-y_min) / (y_max-y_min)
-
-    X_train, X_test, y_train, y_test = train_test_split(x_scaled, y_data, test_size=0.3, random_state=10, stratify=y_data_c)
-
-    plt.figure(figsize=(10, 10))
-
-    # Use SVR regression
-    regressor = SVR(C=0.2, kernel='linear', gamma='auto', degree=2, tol=0.0001)
-    model_process_rg(regressor, X_train, X_test, y_train, y_test, "SVR Regression", 1, y_max, y_min)
-
-    # Use Random Forest regression
-    regressor = RandomForestRegressor(n_estimators=20, max_depth=7)
-    model_process_rg(regressor, X_train, X_test, y_train, y_test, "Random Forest Regression", 2, y_max, y_min)
-
-    # Use MLPRegressor solver:{‘lbfgs’, ‘sgd’, ‘adam’} activation:{‘identity’, ‘logistic’, ‘tanh’, ‘relu’}
-    regressor = MLPRegressor(hidden_layer_sizes=(5, 3), activation="identity", solver="lbfgs",
-                             max_iter=10000, random_state=3, early_stopping=True)
-    model_process_rg(regressor, X_train, X_test, y_train, y_test, "MLP Regression", 3, y_max, y_min)
-
-    # Use GradientBoostingRegressor
-    regressor = GradientBoostingRegressor(learning_rate=1, n_estimators=70, max_depth=2,
-                                          random_state=2, loss="huber", validation_fraction=0.2)
-    model_process_rg(regressor,X_train, X_test, y_train, y_test, "GradientBoosting Regression", 4, y_max, y_min)
-
-    plt.show()
+    return clf
 
 
 def model_process_rg(rg, X_train, X_test, y_train, y_test, method, plot_num, y_max, y_min):
@@ -142,32 +117,6 @@ def model_process_rg(rg, X_train, X_test, y_train, y_test, method, plot_num, y_m
     output.to_csv(f"{method}.csv")
 
     return rg
-
-
-def model_process(clf, X_train, X_test, y_train, y_test, method, x_scaled, y_data):
-    # 開始時間
-    start = time.process_time()
-
-    # 訓練機器學習模型
-    clf.fit(X_train, y_train)
-
-    # 計算測試集上的準確率、精確率、召回率和F1值
-    accuracy = clf.score(X_test, y_test)
-    precision, recall, f1, _ = precision_recall_fscore_support(y_test, clf.predict(X_test), average='macro')
-
-    # 計算混淆矩陣
-    conf_matrix = confusion_matrix(y_test, clf.predict(X_test))
-
-    # 顯示訓練結果
-    print(f"{method} - Training Time: {time.process_time() - start:.3f} s, Accuracy: {accuracy * 100:.1f}, Precision: {precision * 100:.1f}, Recall: {recall * 100:.1f}, F1:{f1 * 100:.1f}")
-    print("Confusion Matrix: ")
-    print(conf_matrix)
-
-    # 計算並顯示交叉驗證分數
-    print(f"{method} - Cross-Validation Score: {np.average(cross_val_score(clf, x_scaled, y_data, cv=5))}")
-
-    # 返回已經訓練好的機器學習模型
-    return clf
 
 
 def PCA_preprocess(data: object) -> object:
